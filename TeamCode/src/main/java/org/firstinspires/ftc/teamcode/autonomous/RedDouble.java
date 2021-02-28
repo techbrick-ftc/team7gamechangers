d78d571b4a4f5d212d8875ebffaa3021991bf56a
naeBrettuB <gavinsavage24@gmail.com>
2021-02-28 11:03:23 -0500

    in-comp changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedDouble.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedDouble.java
@@ -13,36 +13,88 @@ public class RedDouble extends AutoImport {
         super.runOpMode();
 
         if (opModeIsActive()) {
-            // drives to shooting position and shoots 3 rings
-            shooter.setVelocity(-1600);
-            if (activeGoal != 0) { slauto.drive(9, 27, 0, 1, 0, this, false, true); }
-            slauto.drive(0, 15, -18, 1, this);
-            shoot(-1550, 3, 0, 500, true);
+            if (activeGoal != 2) {
+                // drives to shooting position and shoots 3 rings
+                shooter.setVelocity(-1600);
+                if (activeGoal != 0) {
+                    slauto.drive(9, 27, 0, 1, 0, this, false, true);
+                }
+                slauto.drive(0, 15, -18, 1, this);
+                shoot(-1550, 3, 0, 500, true);
 
-            // does the following if there are rings on field
-            if (activeGoal != 0) {
-                slauto.drive(10, 15, 0, 1, this);
-                sleep(2000);
-            }
-            if (activeGoal == 1) {
-                // picks up single ring
-                slauto.drive(0, 40, 0, 1, this);
-                intakeControl("in");
-                slauto.drive(17, 40, 0, 0.3, 5, this, false, true);
+                // does the following if there are rings on field
+                if (activeGoal != 0) {
+                    slauto.drive(10, 15, 0, 1, this);
+                    sleep(2000);
+                }
+                if (activeGoal == 1) {
+                    // picks up single ring
+                    slauto.drive(0, 40, 0, 1, this);
+                    intakeControl("in");
+                    slauto.drive(17, 40, 0, 0.3, 5, this, false, true);
+                    sleep(500);
+                    intakeControl("off");
+
+                    // drives to shooting position
+                    shooter.setVelocity(-1500);
+                    slauto.drive(2, 39, 0, 1, 0, this, false, false);
+
+                    // shoots
+                    shoot(-1500, 1, 1000, 100, true);
+                    shooter.setVelocity(0);
+
+                } else if (activeGoal == 2) {
+                    // knocks down stack of rings, and picks 3 up
+                    slauto.drive(0, 40, 0, 1, this);
+                    slauto.drive(10, 40, 0, 1, 0, this, false, false); // Knocks Stack
+                    slauto.drive(7, 40, 0, 1, 0, this, false, false);
+                    intakeControl("in");
+                    slauto.drive(24, 40, 0, 0.3, 5, this, false, true);
+                    sleep(500);
+                    intakeControl("off");
+
+                    // drives to shooting position and shoots
+                    shooter.setVelocity(-1600);
+                    slauto.drive(2, 39, 0, 1, this);
+                    shoot(-1500, 3, 0, 500, true);
+                    shooter.setVelocity(0);
+                }
+
+                slauto.drive(35, 28, 180, 1, this);
+                if (activeGoal != 2) {
+                    sleep(2000);
+                }
+                if (activeGoal == 0) {
+                    sleep(2500);
+                }
+
+                // drives to wobble goal and drops, before raising again
+                wobbleAsync(6500, 1, 1, "red", activeGoal, slauto, this);
+                wobbleMove(true, this, telemetry);
                 sleep(500);
-                intakeControl("off");
+                wobbleManual(3050, 1);
+                sleep(200);
+                if (activeGoal == 1) {
+                    sleep(800);
+                }
 
-                // drives to shooting position
-                shooter.setVelocity(-1500);
-                slauto.drive(2, 39, 0, 1, 0, this, false, false);
+                // parks
+                if (activeGoal != 1) {
+                    slauto.drive(0, -7, 180, 1, this);
+                } else {
+                    slauto.drive(0, 46, 180, 1, this);
+                }
 
-                // shoots
-                shoot(-1500, 1, 1000, 100, true);
-                shooter.setVelocity(0);
+                // If there are 4 rings
+            } else {
+                // drives to shooting position and shoots 3 rings
+                shooter.setVelocity(-1600);
+                if (activeGoal != 0) { slauto.drive(9, 27, 0, 1, 0, this, false, true); }
+                slauto.drive(2, 39, 0, 1, this);
+                shoot(-1500, 3, 0, 500, true);
 
-            } else if (activeGoal == 2) {
                 // knocks down stack of rings, and picks 3 up
-                slauto.drive(0, 40, 0, 1, this);
+                slauto.drive(4, 40, 0, 1, this);
                 slauto.drive(10, 40, 0, 1, 0, this, false, false); // Knocks Stack
                 slauto.drive(7, 40, 0, 1, 0, this, false, false);
                 intakeControl("in");
@@ -55,25 +107,17 @@ public class RedDouble extends AutoImport {
                 slauto.drive(2, 39, 0, 1, this);
                 shoot(-1500, 3, 0, 500, true);
                 shooter.setVelocity(0);
-            }
 
-            slauto.drive(35, 28, 180, 1, this);
-            if (activeGoal != 2) { sleep(2000); }
-            if (activeGoal == 0) { sleep(2500); }
+                // drives to wobble goal and drops, before raising again
+                wobbleAsync(6500, 1, 1, "red", activeGoal, slauto, this);
+                wobbleMove(true, this, telemetry);
+                sleep(500);
+                wobbleManual(3050, 1);
+                sleep(200);
 
-            // drives to wobble goal and drops, before raising again
-            wobbleAsync(6500, 1, 1, "red", activeGoal, slauto, this);
-            wobbleMove(true, this, telemetry);
-            sleep(500);
-            wobbleManual(3050, 1);
-            sleep(200);
-            if (activeGoal == 1) { sleep(800); }
-
-            // parks
-            if (activeGoal != 1) {
-                slauto.drive(0, -7, 180, 1, this);
-            } else {
-                slauto.drive(0, 46, 180, 1, this);
+                tapeMeasure.setPower(1); // starts extending tape measure to park
+                sleep(4000);
+                tapeMeasure.setPower(0);
             }
         }
     }
